3e4281793c009179971322627c01b1cc97cd9bf0
joao-ramires-esteves <pedromaluco2001@gmail.com>
2021-10-05 12:52:13 -0300

    q5: terminar de renomear

--- q5/src/deque.c
+++ q5/src/deque.c
@@ -4,53 +4,53 @@
 #include <string.h>
 #include "deque.h"
 
-struct fila
+struct deque
 {
   char no[MAX_ELEMS][MAX_CHAR_STR];
   int ini, fim; // fim aponta pro primeiro espaco disponivel
 };
 
-Fila cria_fila ()
+Deque cria_deque ()
 {
-  Fila f = (Fila) malloc( sizeof(struct fila) );
-  if ( f != NULL)
+  Deque d = (Deque) malloc( sizeof(struct deque) );
+  if ( d != NULL)
   {
-    f->ini = 0;
-    f->fim = 0;
+    d->ini = 0;
+    d->fim = 0;
   }
-  return f;
+  return d;
 }
 
-int fila_vazia(Fila f)
+int deque_vazio(Deque d)
 {
-  return (f->ini == f->fim);
+  return (d->ini == d->fim);
 }
 
-int fila_cheia(Fila f)
+int deque_cheio(Deque d)
 {
-  return ( f->ini == ( (f->fim+1) % MAX_ELEMS ) );
+  return ( d->ini == ( (d->fim+1) % MAX_ELEMS ) );
 }
 
-int insere_fim(Fila f, char *elem)
+int insere_fim(Deque d, char *elem)
 {
-  if (fila_cheia(f))
+  if (deque_cheio(d))
   {
     return 0;
   }
   
-  strcpy( f->no[f->fim], elem ); // insere no final
-  f->fim = (f->fim+1) % MAX_ELEMS; // incremento circular
+  strcpy( d->no[d->fim], elem ); // insere no final
+  d->fim = (d->fim+1) % MAX_ELEMS; // incremento circular
   return 1;
 }
 
-int remove_ini(Fila f, char *elem)
+int remove_ini(Deque d, char *elem)
 {
-  if (fila_vazia(f))
+  if (deque_vazio(d))
   {
     return 0;
   }
 
-  strcpy( elem, f->no[f->ini]); // retorno impl
-  f->ini = (f->ini+1) % MAX_ELEMS; // incremento circular
+  strcpy( elem, d->no[d->ini]); // retorno impl
+  d->ini = (d->ini+1) % MAX_ELEMS; // incremento circular
   return 1;
 }
