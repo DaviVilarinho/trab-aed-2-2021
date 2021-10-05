8415c8d6002a96607a80e45cfff1cee95bff3265
joao-ramires-esteves <pedromaluco2001@gmail.com>
2021-10-05 14:51:49 -0300

    q5: formatacao .h

--- q5/src/deque.h
+++ q5/src/deque.h
@@ -6,8 +6,12 @@ typedef struct deque * Deque;
 Deque cria_deque ();
 int deque_vazio(Deque d);
 int deque_cheio(Deque d);
+
 int insere_ini(Deque d, char *elem);
 int insere_fim(Deque d, char *elem);
 
 int remove_ini(Deque d, char *elem);
 int remove_fim(Deque d, char *elem);
+
+int apaga_deque(Deque d);
+int esvazia_deque(Deque d);
