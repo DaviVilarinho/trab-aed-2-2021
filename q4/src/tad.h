71f404858bb36dce42928c322b8e1ed3c982bb06
Appa <72236662+AppaTico@users.noreply.github.com>
2021-10-06 22:18:13 -0300

    Update tad.h

---
+++ q4/src/tad.h
@@ -0,0 +1,34 @@
+struct produtos
+{
+    int codigo;
+    char descricao[100];
+    float valor;
+    int validade;
+};
+
+struct fila
+{
+    struct no *ini, *fim;
+};
+
+typedef struct produtos Produto;
+typedef struct fila * Fila;
+
+struct no
+{
+    Produto info;
+    struct no *prox;
+    int contador;
+};
+
+Fila cria_fila ();
+
+int fila_vazia (Fila);
+
+int insere_asc (Fila, Produto);
+
+int remove_ini (Fila, Produto *);
+
+int apaga_fila (Fila);
+
+int esvazia_fila (Fila);
