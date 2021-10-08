4244a6d74378b97f6753951104c5897ecbd7f1b3
joao-ramires-esteves <pedromaluco2001@gmail.com>
2021-10-07 21:42:21 -0300

    remover feito, cobrar quase certo

--- q3/src/app.c
+++ q3/src/app.c
@@ -1,6 +1,5 @@
 /*
  * TODO:
- *  - PRINTA CARRO VERIFICA PRECO
  *  - RETIRAR VEICULO
  */
 #include "fila.h"
@@ -11,17 +10,24 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include <string.h>
 #define NUMERO_BOXES      6 // 6 porque o de espera é o 0
 #define PRIMEIRO_BOX_UTIL 1
 #define BOX_ESPERA        0 
 
+int nao_percorridos(int tamanhos[]);
 void print_menu();
 void print_carro(carro_t carro);
 int insere_no_melhor_box(fila_p boxes[], carro_t inserir, int isFilaDeEspera); 
-  // >0 -> inseriu, -1 -> fila de espera, 0 -> nao inseriu
+  // >0 -> inseriu (em qual box entrou), -1 -> fila de espera, 0 -> nao inseriu
 
 int main (void) {
   fila_p boxes[NUMERO_BOXES];
+  carro_t inserindo;
+  carro_t removendo;
+  char placa_remover[8];
+  int tamanhos[5];
+  int FLAG_CICLO_REMOVER;
   for (int i = 0; i < NUMERO_BOXES; i++) 
     boxes[i] = cria_fila(); // inicializa a fila dos boxes
   
@@ -48,7 +54,6 @@ int main (void) {
       // entrada
       case 1:
         // leitura do carro
-        carro_t inserindo;
         printf("PLACA: ");
         scanf("%[^\n]", inserindo.placa); setbuf(stdin, NULL);
         printf("TIPO DE SERVICO (A-avulso, M-mensal): ");
@@ -73,6 +78,49 @@ int main (void) {
 
       // saida
       case 2: 
+        // leitura do carro
+        printf("PLACA: ");
+        scanf("%[^\n]", placa_remover); setbuf(stdin, NULL);
+        
+        for (int i = PRIMEIRO_BOX_UTIL; i < NUMERO_BOXES; i++) {
+          tamanhos[i-1] = tamanho(boxes[i]);
+        }
+
+        FLAG_CICLO_REMOVER = 1;
+        while (nao_percorridos(tamanhos) != 0 && (FLAG_CICLO_REMOVER)) {
+          for (int cabeca_box = PRIMEIRO_BOX_UTIL; cabeca_box < NUMERO_BOXES; cabeca_box++) {
+            if (tamanhos[cabeca_box-1] != 0) {
+              remove_ini(boxes[cabeca_box], &removendo);
+              if (strcmp(removendo.placa, placa_remover) == 0) {
+                printf("\n");
+                printf("carro removido: ");
+                print_carro(removendo);
+                printf("\n");
+
+                // calculo de quanto pagar
+                time_t timer; time(&timer);
+                double tempo_transcorrido = difftime(timer, removendo.hora); // sai em segundos
+                // 5 reais a primeira hora + 1,50 cada hora adicional (tolerancia 10s)
+                double minuto_inicial = (tempo_transcorrido >= 60.00) ? 1.00: 0.00;
+                double minutos_adicionais = 0.00;
+                if (tempo_transcorrido >= 70) {
+                  minutos_adicionais = tempo_transcorrido - 70.00;
+                }
+                double pagar_total = (5.00 * minuto_inicial) + (1.50 * minutos_adicionais);
+                printf("pagar: R$%.2lf", pagar_total);
+                FLAG_CICLO_REMOVER = 0; 
+                break;
+              } else {
+                insere_fim(boxes[cabeca_box], removendo);
+                printf("nao eh o carro: ");
+                print_carro(removendo);
+                printf("\n");
+              } 
+
+              tamanhos[cabeca_box-1] -= 1;
+            }
+          }
+        }
         break;
 
       // visualizacao
@@ -155,4 +203,16 @@ int insere_no_melhor_box(fila_p boxes[], carro_t inserir, int isFilaDeEspera) {
       return 0;
     }
   }
+}
+
+int nao_percorridos(int tamanhos[]) {
+  int num_boxes_nao_percorridos = 0;
+  
+  for (int i = PRIMEIRO_BOX_UTIL-1; i < NUMERO_BOXES-1; i++) {
+    if (tamanhos[i] != 0) {
+      num_boxes_nao_percorridos++; 
+    }
+  }
+
+  return num_boxes_nao_percorridos;
 }
