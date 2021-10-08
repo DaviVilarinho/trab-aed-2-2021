eb0be221debb2647e801eff672a0c96303c0a4d0
vilarinux <davi.r.vilarinho@gmail.com>
2021-10-08 11:33:06 -0300

    adicionado tads

---
+++ q2/src/pilha.h
@@ -0,0 +1,13 @@
+#define MAX_NOME 30
+
+struct no;
+
+typedef struct no * pilha_p;
+
+pilha_p cria_pilha();
+int pilha_vazia(pilha_p pilha);
+int push(pilha_p *pilha_ref, void *ptr);
+int pop(pilha_p *pilha_ref, void **ptr);
+int le_topo(pilha_p pilha, void **ptr);
+int libera_pilha(pilha_p *pilha_ref);
+int esvazia_pilha(pilha_p *pilha_ref);
